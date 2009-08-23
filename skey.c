87ae1319fcaed16d28367a4e207dba0d0edbc05b
William R. Fraser <wfraser@galileo.home>
2009-08-22 23:11:25 -0500

    adding a readme and some legal

--- skey.c
+++ skey.c
@@ -1,10 +1,18 @@
 /*
  * S/Key OTP Calculator (RFC 2289)
  * 
- * by William R. Fraser 7/20/200
+ * by William R. Fraser 7/20/2009
  *
  * compile with:
  *	gcc -Wall -o skey -lmhash -lm skey.c
+ *
+ * usage: skey [otp-<hash>] <rounds> <seed>
+ *
+ * You will be prompted for your secret password, and then skey will print the
+ * one-time password in hexadecimal and six-word form.
+ *
+ * For restrictions regarding usage and distribution, see license at the end of
+ * this file.
  */
 
 #include <stdio.h>
@@ -30,7 +38,7 @@ void hash_finalize(int hash, char *input, char **output)
 			(*output)[i] = input[i] ^ input[i+8];
 		}
 		break;
-	case MHASH_SHA1:
+	case MHASH_SHA1: //XXX THIS DOES NOT WORK AND I DO NOT KNOW WHY
 		for (i = 0; i < 8; i++) {
 			(*output)[i] = input[i] ^ input[i+8];
 		}
@@ -151,6 +159,7 @@ void hash_break(void *hash, unsigned long chunks[6])
 	chunks[5] |= checksum & 3;
 }
 
+#if 0
 /*
  * Another way of doing this, using unsigned long longs. This method sucks.
  */
@@ -188,6 +197,7 @@ void xx_hash_break(unsigned long long int hash, unsigned long chunks[6])
 	}
 	chunks[5] |= checksum & 3;
 }
+#endif //0
 
 int main(int argc, char **argv, char **envp)
 {
@@ -256,3 +266,32 @@ int main(int argc, char **argv, char **envp)
 	return 0;
 }
 
+/*
+
+Copyright (c) 2009, William R. Fraser
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of William R. Fraser nor the names of other
+      contributors may be used to endorse or promote products derived from
+      this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY William R. Fraser ''AS IS'' AND ANY
+EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL <copyright holder> BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+vim: sts=8 ts=8 noexpandtab
+*/
